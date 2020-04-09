cb3a56dacc5088684ce6f89483fab6c76035cd6a
Dan Serbyn <dev.serbyn@gmail.com>
2020-04-09 15:00:45 +0300

    Add logging support

---
+++ src/main/java/com/devserbyn/suncheck/listener/StartupApplicationListener.java
@@ -0,0 +1,25 @@
+package com.devserbyn.suncheck.listener;
+
+import com.devserbyn.suncheck.constant.PATH_CONSTANT;
+import com.devserbyn.suncheck.utility.ResourceUtil;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class StartupApplicationListener implements ApplicationListener<ApplicationStartedEvent> {
+
+    private final ResourceUtil resourceUtil;
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        // Print banner saying bot initialized
+        System.out.println(resourceUtil.readResourceFileLines(PATH_CONSTANT.BANNER_BOT_INIT));
+    }
+}
