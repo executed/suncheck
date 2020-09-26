8b178440b5cd6679ec1908f6618a5e8cde6c2e3a
Dan Serbyn <dev.serbyn@gmail.com>
2020-09-26 16:52:38 +0300

    Implement bot commands configuring

--- src/main/java/com/devserbyn/suncheck/constant/PATH_CONSTANT.java
+++ src/main/java/com/devserbyn/suncheck/constant/PATH_CONSTANT.java
@@ -3,4 +3,5 @@ package com.devserbyn.suncheck.constant;
 public final class PATH_CONSTANT {
 
     public static final String BANNER_BOT_INIT = "banner-bot-initialized.txt";
+    public static final String BOT_COMMANDS = "bot_commands.txt";
 }
--- src/main/java/com/devserbyn/suncheck/model/SuncheckBot.java
+++ src/main/java/com/devserbyn/suncheck/model/SuncheckBot.java
@@ -1,23 +1,34 @@
 package com.devserbyn.suncheck.model;
 
+import com.devserbyn.suncheck.constant.PATH_CONSTANT;
 import com.devserbyn.suncheck.constant.STR_CONSTANT;
 import com.devserbyn.suncheck.controller.ApiController;
 import com.devserbyn.suncheck.service.BotErrorHandler;
+import com.devserbyn.suncheck.utility.ResourceUtil;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class SuncheckBot extends TelegramLongPollingBot {
 
     private final ApiController apiController;
     private final BotErrorHandler botErrorHandler;
+    private final ResourceUtil resourceUtil;
 
     private boolean silentMessage = false;
 
@@ -53,5 +64,20 @@ public class SuncheckBot extends TelegramLongPollingBot {
 
     public void setSilentMessage(boolean silentMessage) {
         this.silentMessage = silentMessage;
+    }
+
+    public void setMyCommands() {
+        final String[] fileLines = resourceUtil.readResourceFileLines(PATH_CONSTANT.BOT_COMMANDS).split("\n");
+        List<BotCommand> botCommandList = new ArrayList<>();
+        Arrays.stream(fileLines).forEach(l -> {
+            final String[] commandAndDesc = l.split(" - ");
+            botCommandList.add(new BotCommand(commandAndDesc[0], commandAndDesc[1]));
+        });
+        try {
+            this.execute(new SetMyCommands(botCommandList));
+            log.info("Bot commands were successfully set");
+        } catch (TelegramApiException e) {
+            botErrorHandler.handleSendingMessageException(1L, "setCommands", e);
+        }
     }
 }
